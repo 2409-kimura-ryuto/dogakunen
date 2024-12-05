d6e5f534d0a7c20f891d80d7eb4c630574e6a169
kimura_ryuto <kimura.ryuto@alhinc.jp>
2024-12-05 10:29:19 +0900

    first commit

---
+++ src/main/java/com/example/dogakunen/DogakunenApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogakunen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogakunenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogakunenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dogakunen/DogakunenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogakunen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogakunenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
