2a580dc391a14e7aed71b9bf856e488d9fe0aefb
saito_shuhei <saito.shuhei@alhinc.jp>
2024-12-06 10:18:23 +0900

    微修正

--- src/main/java/com/example/dogakunen/controller/AttendanceController.java
+++ src/main/java/com/example/dogakunen/controller/AttendanceController.java
@@ -1,13 +1,21 @@
 package com.example.dogakunen.controller;
 
+import com.example.dogakunen.controller.form.UserForm;
+import com.example.dogakunen.service.DateAttendanceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AttendanceController {
+    @Autowired
+    DateAttendanceService dateAttendanceService;
+
     @GetMapping
-    public String top() {
+    public String top(UserForm loginUser) {
+        Integer loginId = loginUser.getId();
+        dateAttendanceService.findALLAttendances(loginId);
         return "/home";
     }
 }
