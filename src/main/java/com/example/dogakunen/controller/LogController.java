f10060f0bc560b9d64b3aed34e0de0801943a019
miura_ami <miura.ami@alhinc.jp>
2024-12-20 14:02:13 +0900

    承認・差し戻し後の画面遷移、ページネーション追加

--- src/main/java/com/example/dogakunen/controller/LogController.java
+++ src/main/java/com/example/dogakunen/controller/LogController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
